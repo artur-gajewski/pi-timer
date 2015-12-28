fe39ef23fb23f5a9e3ed1fc53f01d6a08f219d94
BuildTools <unconfigured@null.spigotmc.org>
2015-12-28 22:10:43 +0200

    Initial

---
+++ timer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+# Mode assignments
+ON = GPIO.LOW
+OFF = GPIO.HIGH
+
+# LED assignment
+RedPin = 11
+GreenPin = 12
+
+# Timer setting in minutes
+seconds = 60 * 8
+
+# GPIO settings
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(RedPin, GPIO.OUT)
+GPIO.output(RedPin, OFF)
+GPIO.setup(GreenPin, GPIO.OUT)
+GPIO.output(GreenPin, OFF)
+
+try:
+	while True:
+		print 'The oven is hot, baking...'
+		GPIO.output(RedPin, ON)
+		GPIO.output(GreenPin, OFF)
+		time.sleep(seconds)
+		print 'Stuff is ready!'
+		GPIO.output(RedPin, OFF)
+		GPIO.output(GreenPin, ON)
+		while True:
+			print "Take the stuff out of the oven!"
+			time.sleep(10)
+
+# When Ctrl+C is pressed...
+except KeyboardInterrupt:
+	GPIO.output(RedPin, OFF)
+        GPIO.output(GreenPin, OFF)
+	GPIO.cleanup()
+
