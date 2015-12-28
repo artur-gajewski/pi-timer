b727e53f7582fe888892da51bb64633df449bcc1
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2015-12-28 22:38:16 +0200

    Refactored code

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 Oven timer for Raspberry Pi
 ============================
 
-Have you ever wanted to go watch TV while you need to bake something? Have you ever forgotten about the stuff
+Have you ever wanted to go watch TV while you needed to bake something in the oven? Have you ever forgotten about the stuff
 in the oven and burned it while watching sports?
 
 Why not putting your Raspberry Pi in use and build a LED timer so that next time your buns are in the oven, you
-get notified with red or green LEDs about whether the buns are done or not?
+get notified with red or green LEDs about whether the buns are done or not? Place your Pi next to your TV and you
+will see green light before you smell burned food ;-)
+
+### In the oven
 
 ![The oven is hot!](/images/baking.jpg)
 
+### Buns are ready!
+
 ![Time to take them out from the oven!](/images/done.jpg)
 
+### Time to om nom nom!
+
 ![Om nom nom nom!](/images/result.jpg)
+
+### Connections
 
 ![Connections](/images/timer.png)
--- timer.py
+++ timer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import RPi.GPIO as GPIO
 import time
 
@@ -10,7 +11,8 @@ OFF = GPIO.HIGH
 RedPin = 11
 GreenPin = 12
 
-# Timer setting in minutes
+# Timer setting in minutes, adjust to your needs
+# Here time is set for eight minutes
 seconds = 60 * 8
 
 # GPIO settings
@@ -21,21 +23,24 @@ GPIO.setup(GreenPin, GPIO.OUT)
 GPIO.output(GreenPin, OFF)
 
 try:
-	while True:
-		print 'The oven is hot, baking...'
-		GPIO.output(RedPin, ON)
-		GPIO.output(GreenPin, OFF)
-		time.sleep(seconds)
-		print 'Stuff is ready!'
-		GPIO.output(RedPin, OFF)
-		GPIO.output(GreenPin, ON)
-		while True:
-			print "Take the stuff out of the oven!"
-			time.sleep(10)
+    while True:
+        print 'The oven is hot, baking...'
+        GPIO.output(RedPin, ON)
+        GPIO.output(GreenPin, OFF)
+
+        time.sleep(seconds)
+
+        print 'Stuff is ready!'
+        GPIO.output(RedPin, OFF)
+        GPIO.output(GreenPin, ON)
+
+        while True:
+            print "Take the stuff out of the oven!"
+            time.sleep(10)
 
 # When Ctrl+C is pressed...
 except KeyboardInterrupt:
-	GPIO.output(RedPin, OFF)
-        GPIO.output(GreenPin, OFF)
-	GPIO.cleanup()
+    GPIO.output(RedPin, OFF)
+    GPIO.output(GreenPin, OFF)
+    GPIO.cleanup()
 
